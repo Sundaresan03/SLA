cae7f3f901b30a52ea4bb0fa0b75ead02cdbffa7
Sundaresan03 <129873227+Sundaresan03@users.noreply.github.com>
2026-07-13 16:02:21 +0530

    Add files via upload

---
+++ Day15Task.js
@@ -0,0 +1,22 @@
+// 3.Log your name and age in console.
+let name="Sundaresan B";
+let age=23;
+console.log(`Name is ${name} and age is ${age}`);
+//4.Write single-line and multi-line comments.
+//This is single line comment
+/* This is 
+   multi line
+   comment*/
+
+//5.Declare variables using var, let, and const.
+var firstname="Jacob";
+let myage=22;
+const pi=3.14;
+
+//6.Create variables with string, number, boolean, null, undefined.
+let nameString="Mahesh";
+let number=374;
+let booleanValue=true;
+let variable=null;
+let something;//undefined
+
